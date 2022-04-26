105cf01562a84de532fbff633147d376206ae9ac
BiancaPalileo <biclpalileo@gmail.com>
2022-04-26 09:22:55 +0100

    Starting Nematode Segments

--- GE2 Test 2022 Starter Unity/Assets/Nematode.cs
+++ GE2 Test 2022 Starter Unity/Assets/Nematode.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 public class Nematode : MonoBehaviour
 {
     public int length = 5;
+    public GameObject spherePrefab;
+    // public float distance = 1;
 
     public Material material;
 
     void Awake()
-    {
+    {        
         // Put your code here!
+        length = Random.Range(2, 7);
+
+        for(var i = 0; i < length; i++) {
+            GameObject segment = Instantiate(spherePrefab, transform.position, transform.rotation, gameObject.transform);
+            segment.transform.position = transform.position - (transform.forward * i);
+        }
+
+
     }
 
 
